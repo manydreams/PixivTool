86fbb88dbb072c5ca5d94304ea37e7dfb1ae62a7
damberva <2836145041@qq.com>
2025-07-30 23:53:05 +0800

    Refactor Cache class methods for improved timeout handling and code clarity

--- cache.py
+++ cache.py
@@ -12,7 +12,18 @@ class Cache:
         self.max_size = max_size
         self.cache = {}
         self.cache_timeout: list[tuple[float, str]] = []
-        
+    
+    def _is_timeout(self, key: Any) -> bool:
+        """Check if the cache entry for the given key is timed out.
+
+        Args:
+            key (Any): The key to check in the cache.
+
+        Returns:
+            bool: True if the cache entry is timed out, False is not timeout.
+        """
+        return self.cache[key][1] < datetime.datetime.now().timestamp()
+
     def clear_timeout(self) -> int:
         now = datetime.datetime.now().timestamp()
         cnt = 0
@@ -29,34 +40,38 @@ class Cache:
         self.cache.clear()
     
     def get(self, key: str) -> Any | None:
-        self.clear_timeout()
-        if key in self.cache.keys():
-            _log.debug(f"Cache hit: {key}")
-            return self.cache[key][0]
-        return None
+        if key not in self.cache.keys():
+            return None
+        if self._is_timeout(key):
+            _log.debug(f"Cache entry for {key} is timed out.")
+            self.remove(key, no_check=True)
+            return None
+        _log.debug(f"Cache hit: {key}")
+        return self.cache[key][0]
 
     def update(self, key: str, value: Any,
                timeout: int = default_timeout) -> None:
         self.clear_timeout()
         now = datetime.datetime.now().timestamp()
+        
         if key in self.cache.keys():
             self.cache_timeout.remove((self.cache[key][1], key))
             self.cache[key] = (value, now + timeout)
             bisect.insort(self.cache_timeout, (now + timeout, key),
                           key=lambda x: -x[0])
-            if len(self.cache) > self.max_size:
+            while len(self.cache) > self.max_size:
                 key = self.cache_timeout.pop()[1]
                 del self.cache[key]
         else:
             self.cache[key] = (value, now + timeout)
             bisect.insort(self.cache_timeout, (now + timeout, key),
                           key=lambda x: -x[0])
-            if len(self.cache) > self.max_size:
+            while len(self.cache) > self.max_size:
                 key = self.cache_timeout.pop()[1]
                 del self.cache[key]
         _log.debug(f"Cache updated: {key}")
-            
-    def remove(self, key: str):
-        if key in self.cache:
+          
+    def remove(self, key: str, no_check: bool = False):
+        if no_check or key in self.cache:
             self.cache_timeout.remove((self.cache[key][1], key))
             del self.cache[key]
